e212536b322b23ed64d8f9deb8dea8ad10ff8b18
pimax <m.pinyugin@gmail.com>
2013-01-23 16:54:35 +0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 yii-sms
 =======
+
+Yii-расширение для работы с api сервиса [sms.ru](http://sms.ru)
+
+# Установка
+
+Загрузите yii-sms из этого репозитория github:
+
+    cd protected/extensions
+    git clone git://github.com/pimax/yii-sms.git
+
+В protected/config/main.php внесите следующие строки:
+
+    'components' => array
+    (
+        'sms' => array
+        (
+            'class'    => 'application.extensions.yii-sms.Sms',
+            'login'     => 'username',      // Логин на сайте sms.ru
+            'password'   => 'password',     // Пароль
+        )
+    );
+
+# Использование
+
+Отправка SMS:
+
+    Yii::app()->sms->send('79251234567', 'Проверка отправки');
+	Yii::app()->sms->send('79251234567', 'Проверка отправки', 'Имя отправителя', time(), $test = true, $partner_id);
+
+Статус SMS:
+
+    Yii::app()->sms->status('sms id');
+
+Стоимость SMS:
+
+    Yii::app()->sms->cost('79251234567', 'Проверка отправки');
+
+Баланс:
+
+    Yii::app()->sms->balance();
+
+Дневной лимит:
+
+    Yii::app()->sms->limit();
+
+Отправители:
+
+    Yii::app()->sms->senders();
+
+Проверка валидности логина и пароля:
+
+    Yii::app()->sms->check();
---
+++ yii-sms/Sms.php
@@ -0,0 +1,239 @@
+<?php
+
+class Sms extends CApplicationComponent
+{
+    const HOST  = 'http://sms.ru/';
+	const SEND = 'sms/send?';
+	const STATUS = 'sms/status?';
+	const COST = 'sms/cost?';
+	const BALANCE = 'my/balance?';
+	const LIMIT = 'my/limit?';
+	const SENDERS = 'my/senders?';
+	const GET_TOKEN = 'auth/get_token';
+	const CHECK = 'auth/check?';
+    
+    public $login;
+    
+    public $password;
+    
+    public $token;
+    
+    public $id;
+    
+    public $sha512;
+    
+    /**
+     * Инициализация компонента
+     * 
+     * @throws CException
+     */
+    public function init()
+	{
+		if (!function_exists ('curl_init'))
+        {
+            throw new CException ('Для работы расширения требуется cURL');
+        }
+
+		parent::init();
+	}
+    
+    /**
+     * Send message
+     * 
+     * @param string $to 
+     * @param string $text 
+     * @param string $from 
+     * @param integer $time 
+     * @param boolean $test 
+     * @param type $partner_id 
+     * @return array
+     */
+    public function send($to, $text, $from = null, $time = null, $test = false, $partner_id = null)
+    {
+        $url = self::HOST . self::SEND;
+		$this->id = null;
+
+		$params = $this->get_default_params();
+		$params['to'] = $to;
+		$params['text'] = $text;
+
+		if ($from)
+			$params['from'] = $from;
+
+		if ($time && $time < (time() + 7 * 60 * 60 * 24))
+			$params['time'] = $time;
+
+		if ($test)
+			$params['test'] = 1;
+
+		if ($partner_id)
+			$params['partner_id'] = $partner_id;
+
+		$result = $this->request($url, $params);
+		$result = explode("\n", $result);
+
+		return array(
+			'code' => $result[0],
+			'id' => $result[1],
+			'balance' => str_replace( 'balance=', '', $result[2] )
+		);
+    }
+    
+    /**
+     * Check message status
+     * 
+     * @param type $id
+     * @return type
+     */
+    public function status($id)
+    {
+        $url = self::HOST.self::STATUS;
+
+		$params = $this->get_default_params();
+		$params['id'] = $id;
+		$result = $this->request($url, $params);
+
+		return $result;
+    }
+    
+    /**
+     * Check user balance
+     * @return array
+     */
+    public function balance()
+    {
+        $url = self::HOST . self::BALANCE;
+
+		$params = $this->get_default_params();
+		$result = $this->request($url, $params);
+		$result = explode("\n", $result);
+
+		return array(
+			'code' => $result[0],
+			'balance' => $result[1]
+		);
+    }
+    
+    /**
+     * Check day limit
+     * 
+     * @return array
+     */
+    public function limit()
+    {
+        $url = self::HOST . self::LIMIT;
+
+		$params = $this->get_default_params();
+		$result = $this->request($url, $params);
+		$result = explode("\n", $result);
+
+		return array(
+			'code' => $result[0],
+			'total' => $result[1],
+			'current' => $result[2]
+		);
+    }
+    
+    /**
+     * Get message cost
+     * 
+     * @param type $to
+     * @param type $text
+     * @return type
+     */
+    public function cost($to, $text) 
+    {
+		$url = self::HOST.self::COST;
+		$this->id = null;
+
+		$params = $this->get_default_params();
+		$params['to'] = $to;
+		$params['text'] = $text;
+
+		$result = $this->request($url, $params);
+		$result = explode("\n", $result);
+
+		return array(
+			'code' => $result[0],
+			'price' => $result[1],
+			'number' => $result[2]
+		);
+	}
+    
+    /**
+     * Get my senders list
+     * 
+     * @return array
+     */
+    public function senders() 
+    {
+		$url = self::HOST . self::SENDERS;
+		$params = $this->get_default_params();
+		$result = $this->request( $url, $params );
+		$result = explode("\n", rtrim($result));
+
+		$response = array(
+			'code' => $result[0],
+			'senders' => $result
+		);
+		unset($response['senders'][0]);
+		$response['senders'] = array_values($response['senders']);
+        
+		return $response;
+	}
+    
+    /**
+     * Check user auth
+     * 
+     * @return type
+     */
+    public function check() 
+    {
+		$url = self::HOST . self::CHECK;
+		$params = $this->get_default_params();
+		$result = $this->request($url, $params);
+
+		return $result;
+	}
+    
+    protected function get_default_params() 
+    {
+        $this->get_auth_token();
+        $this->get_sha512();
+        
+		return array(
+            'login' => $this->login,
+            'token' => $this->token,
+            'sha512' => $this->sha512
+        );
+	}
+    
+    protected function get_auth_token() 
+    {
+		$url = self::HOST . self::GET_TOKEN;
+		$this->token = $this->request($url);
+        
+		return $this->token;
+
+	}
+    
+    protected function get_sha512() 
+    {
+		$this->sha512 = hash('sha512', $this->password.$this->token);
+	}
+    
+    protected function request($url, $params = array()) 
+    {
+		$ch = curl_init($url);
+		$options = array(
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_POSTFIELDS => $params
+		);
+		curl_setopt_array($ch, $options);
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		return $result;
+	}
+}
